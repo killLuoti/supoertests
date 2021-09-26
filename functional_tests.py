4b1312cc4c849b4c337793529e738cfade21cdf6
MyLuo <2542799698@qq.com>
2021-09-26 21:27:32 +0800

    fist test

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,18 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+import unittest
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    def tearDown(self):
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 伊迪丝听说有一个很酷的在线待办事项应用
+        # 她去看了这个应用的首页
+        self.browser.get('http://localhost:8000')
+        # 她注意到网页的标题和头部都包含“To-Do”这个词
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+        # 应用邀请她输入一个待办事项
+        [其余的注释和之前一样]
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
